c5faf961ce5df6a78601f2054d5cd0e681718084
OneWay <37520445+wongil12@users.noreply.github.com>
2026-08-06 12:36:33 +0900

    feat: add deploy github pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGitHubPages ? "/wongil-homepage" : "",
+};
 
 export default nextConfig;
